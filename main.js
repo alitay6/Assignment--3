87717a73a3cfabd7045d0cebba226b7805f54e99
alitay6 <ali.taylor@uleth.ca>
2023-12-08 16:54:25 -0700

    upload all

---
+++ main.js
@@ -0,0 +1,50 @@
+window.onload = function() {
+let canvas = document.getElementById('paper');
+let ctx = canvas.getContext('2d');
+
+canvas.width = 860;
+canvas.height = 855;
+
+let isPainting = false;
+let startX = 0;
+let startY = 0;
+let lineWidth = 5;
+let clearButton = document.getElementById('clear');
+
+clearButton.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+const draw = (e) => {
+    if (!isPainting || e.clientX - canvas.offsetLeft < 110) {
+        return;
+    }
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = '#545454';
+
+    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    ctx.stroke();
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    if (e.clientX - canvas.offsetLeft < 110) {
+        return;
+    }
+    isPainting = true;
+    startX = e.clientX;
+    startY = e.clientY;
+    ctx.beginPath();
+    ctx.moveTo(startX - canvas.offsetLeft, startY - canvas.offsetTop);
+});
+
+canvas.addEventListener('mouseup', e => {
+    isPainting = false;
+    startX = e.clientX;
+    startY = e.clientY;
+    ctx.beginPath();
+    ctx.moveTo(startX - canvas.offsetLeft, startY - canvas.offsetTop);
+});
+
+canvas.addEventListener('mousemove', draw);
+}
